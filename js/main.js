c48764e2d9692a1653ad260d7e177cc9ea5cdca9
Tandel <st34027n@pace.edu>
2016-11-14 16:05:55 -0500

    Padding Changed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 var image;
 var cropper;
 
-
 function validateEmail(sEmail) {
   var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
   if (filter.test(sEmail)) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
@@ -20,13 +18,13 @@ function postError(type, message) {
       $("#messageBox").parent().addClass("alert-danger");
       break;
     case "info":
-    
-      $("#messageBox").html( "<strong>Info!</strong>&nbsp;" + message );
+
+      $("#messageBox").html("<strong>Info!</strong>&nbsp;" + message);
       $("#messageBox").parent().removeClass("alert-danger");
       $("#messageBox").parent().addClass("alert-info");
       break;
     default:
-      $("#messageBox").html( "<strong>Info!</strong>&nbsp;" + message );
+      $("#messageBox").html("<strong>Info!</strong>&nbsp;" + message);
       $("#messageBox").parent().removeClass("alert-danger");
       $("#messageBox").parent().addClass("alert-info");
       break;
@@ -34,20 +32,15 @@ function postError(type, message) {
 }
 
 function clearError() {
-      // $("#messageBox").html( "<strong>Info!</strong>&nbsp;Going great." );
-      $("#messageBox").parent().removeClass("alert-danger");
-      $("#messageBox").parent().removeClass("alert-info");
+  $("#messageBox").html( "<strong>Info!</strong>&nbsp;Going great." );
+  $("#messageBox").parent().removeClass("alert-danger");
+  $("#messageBox").parent().removeClass("alert-info");
 }
 
 window.onload = function () {
   'use strict';
   image = document.getElementById('image');
   var Cropper = window.Cropper;
-
-
-
-       var minAspectRatio = 0.5;
-       var maxAspectRatio = 1.5;
   cropper = new Cropper(image, {
     viewMode: 3,
     dragMode: 'move', //Change to move
@@ -63,54 +56,29 @@ window.onload = function () {
     cropBoxResizable: false, //Change to false
     toggleDragModeOnDblclick: false,
 
-    //                 aspectRatio: 16 / 9,
-    //        autoCropArea: 0.65,
-
-           ready: function () {
-            //  var cropper = this.cropper;
-            //  var containerData = cropper.getContainerData();
-            //  var cropBoxData = cropper.getCropBoxData();
-            //  var aspectRatio = cropBoxData.width / cropBoxData.height;
-            //  var newCropBoxWidth;
-    
-            //  if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio) {
-            //    newCropBoxWidth = cropBoxData.height * ((minAspectRatio + maxAspectRatio) / 2);
-    
-            //    cropper.setCropBoxData({
-            //      left: (containerData.width - newCropBoxWidth) / 2,
-            //      width: newCropBoxWidth
-            //    });
-            //  }
-            $( "#formnoTextBox" ).focus();
-           },
+    ready: function () {
+      $("#formnoTextBox").focus();
+    },
     //          ,
     cropmove: function () {
-               var cropper = this.cropper;
+      var cropper = this.cropper;
       this.cropper.clear()
-
-              //  var cropBoxData = cropper.getCropBoxData();
-              //  var aspectRatio = cropBoxData.width / cropBoxData.height;
-      
-              //  if (aspectRatio < minAspectRatio) {
-              //    cropper.setCropBoxData({
-              //      width: cropBoxData.height * minAspectRatio
-              //    });
-              //  } else if (aspectRatio > maxAspectRatio) {
-              //    cropper.setCropBoxData({
-              //      width: cropBoxData.height * maxAspectRatio
-              //    });
-              //  }
-
       console.log(this.cropper.getCropBoxData());
     }
   });
 
 
-  $(".positive-integer").numeric(
-    { decimal: false, negative: false },
-    function () { alert("Positive integers only"); this.value = ""; this.focus(); });
-$(".numeric").numeric();
-$("#messageBox").html("<strong>Info!</strong> Indicates a neutral informative change or action.");
+  $(".positive-integer").numeric({
+      decimal: false,
+      negative: false
+    },
+    function () {
+      alert("Positive integers only");
+      this.value = "";
+      this.focus();
+    });
+  $(".numeric").numeric();
+  $("#messageBox").html("<strong>Info!</strong> Indicates a neutral informative change or action.");
 
   $("#textboxEmail").blur(function (event) {
     var sEmail = $('#textboxEmail').val();
@@ -123,8 +91,7 @@ $("#messageBox").html("<strong>Info!</strong> Indicates a neutral informative ch
       if (validateEmail(sEmail)) {
         $(this).parent().removeClass("has-error");
         clearError();
-      }
-      else {
+      } else {
         postError('error', 'Invalid Email Address');
         $(this).parent().addClass("has-error");
         event.preventDefault();
@@ -134,42 +101,53 @@ $("#messageBox").html("<strong>Info!</strong> Indicates a neutral informative ch
 
   });
 
-
-	setTimeout(function() {
-
-		// preload image
-		new Image().src = "http://www.attainmentcompany.com/sites/default/files/images/OrderForm-Image.jpg";
+  setTimeout(function () {
+    // preload image
+    new Image().src = "http://www.attainmentcompany.com/sites/default/files/images/OrderForm-Image.jpg";
     new Image().src = "http://www.hotdreamweaver.com/UserFiles/Image/form-to-database.jpg";
 
-	}, 1000);
-
-
+  }, 1000);
 };
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  shortcut("Ctrl+Up",function() {
-  cropper.move(0,-10);
-});
-  shortcut("Ctrl+Down",function() {
-  cropper.move(0,10);
-});
+  shortcut("Ctrl+Up", function () {
+    cropper.move(0, -10);
+  });
+  shortcut("Ctrl+Down", function () {
+    cropper.move(0, 10);
+  });
 
-$( "#formnoTextBox" ).focus(function() {
-  cropper.crop();
-  // cropper.setDragMode("crop");
-  cropper.moveTo(-240);
-  cropper.setCropBoxData({left: 274, top: 134, width: 1321, height: 141});
-});
-$( "#patientnameTextBox" ).focus(function() {
-  // cropper.setDragMode("crop");
-  cropper.crop();
-  cropper.moveTo(-400);
-  cropper.setCropBoxData({left: 527, top: 64, width: 616, height: 115});
-});
+  $("#formnoTextBox").focus(function () {
+    cropper.crop();
+    // cropper.setDragMode("crop");
+    cropper.moveTo(-240);
+    cropper.setCropBoxData({
+      left: 274,
+      top: 134,
+      width: 1321,
+      height: 141
+    });
+  });
+  $("#patientnameTextBox").focus(function () {
+    // cropper.setDragMode("crop");
+    cropper.crop();
+    cropper.moveTo(-400);
+    cropper.setCropBoxData({
+      left: 527,
+      top: 64,
+      width: 616,
+      height: 115
+    });
+  });
 });
 
 function setHighlight(x, y) {
   x.style.background = "yellow";
-  cropper.setCropBoxData({ "left": 100, "top": 200, "width": 100, "height": 300 });
+  cropper.setCropBoxData({
+    "left": 100,
+    "top": 200,
+    "width": 100,
+    "height": 300
+  });
 };
